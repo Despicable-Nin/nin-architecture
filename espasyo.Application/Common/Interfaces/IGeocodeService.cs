76f0822a08f5405c4f89543c094dd95e0819b33b
Ninism <nin.alamo@outlook.com>
2025-02-17 20:00:04 +0800

    Added geocode service

---
+++ espasyo.Application/Common/Interfaces/IGeocodeService.cs
@@ -0,0 +1,6 @@
+﻿namespace espasyo.Application.Common.Interfaces;
+
+public interface IGeocodeService
+{
+    Task<(double? Latitude, double? Longitude)> GetLatLongAsync(string address);
+}
---
+++ espasyo.Infrastructure/Geocoding/AddressGeocodeService.cs
@@ -0,0 +1,31 @@
+﻿using espasyo.Application.Common.Interfaces;
+
+namespace espasyo.Infrastructure.Geocoding;
+
+public class AddressGeocodeService(HttpClient httpClient, ILogger<AddressGeocodeService> logger) : IGeocodeService
+{
+    public async Task<(double? Latitude, double? Longitude)> GetLatLongAsync(string address)
+    {
+        httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("espasyo-WebAPI/1.0");
+        var requestUrl =
+            $"https://nominatim.openstreetmap.org/search?format=json&q={Uri.EscapeDataString(address)}, Philippines";
+        try
+        {
+            var response = await httpClient.GetFromJsonAsync<NominatimResult[]>(requestUrl);
+
+            if (response?.FirstOrDefault() is { } location && 
+                double.TryParse(location.Lat, out var lat) && 
+                double.TryParse(location.Lon, out var lon)) 
+                return (lat, lon);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Exception occurred while making Nominatim request");
+        }
+        
+        return default;
+    }
+
+
+    public record NominatimResult(string Lat, string Lon, string DisplayName);
+}

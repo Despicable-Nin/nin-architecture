8f132842131ab7461c195efaff0cf5e11490b968
Ninism <nin.alamo@outlook.com>
2025-02-17 17:47:10 +0800

    Added events

---
+++ espasyo.Application/Incidents/EventHandlers/IncidentCreatedEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using espasyo.Domain.Events;
+using MediatR;
+
+namespace espasyo.Application.Incidents.EventHandlers;
+
+public class IncidentCreatedEventHandler(ILogger<IncidentCreatedEventHandler> logger) : INotificationHandler<IncidentCreatedEvent>
+{
+    public Task Handle(IncidentCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("Handler invoked", [notification]);
+        return Task.CompletedTask;
+    }
+}

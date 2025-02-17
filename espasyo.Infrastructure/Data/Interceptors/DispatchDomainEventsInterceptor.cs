13e5170a4192dbc5a79ef258cd7accfa3baae378
Ninism <nin.alamo@outlook.com>
2025-02-17 20:04:20 +0800

    Just a cliean up

--- espasyo.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
+++ espasyo.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -5,15 +5,8 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace espasyo.Infrastructure.Data.Interceptors;
 
-public class DispatchDomainEventsInterceptor : SaveChangesInterceptor
+public class DispatchDomainEventsInterceptor(IMediator mediator) : SaveChangesInterceptor
 {
-    private readonly IMediator _mediator;
-
-    public DispatchDomainEventsInterceptor(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
     {
         DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
@@ -46,6 +39,6 @@ public class DispatchDomainEventsInterceptor : SaveChangesInterceptor
         baseEntities.ToList().ForEach(e => e.ClearDomainEvents());
 
         foreach (var domainEvent in domainEvents)
-            await _mediator.Publish(domainEvent);
+            await mediator.Publish(domainEvent);
     }
 }

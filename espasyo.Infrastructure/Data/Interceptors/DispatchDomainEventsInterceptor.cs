09d3e4ea612f926595c3e6946c90eb93d28a7120
Ninism <nin.alamo@outlook.com>
2025-02-17 17:28:13 +0800

    Added interceptor

--- espasyo.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
+++ espasyo.Infrastructure/Data/Interceptors/DispatchDomainEventsInterceptor.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+<<<<<<<
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 
@@ -47,3 +48,58 @@ public class DispatchDomainEventsInterceptor : SaveChangesInterceptor
             await _mediator.Publish(domainEvent);
     }
 }
+
+=======
+using espasyo.Domain.Common;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace espasyo.Infrastructure.Data.Interceptors;
+
+public class DispatchDomainEventsInterceptor : SaveChangesInterceptor
+{
+    private readonly IMediator _mediator;
+
+    public DispatchDomainEventsInterceptor(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        DispatchDomainEvents(eventData.Context).GetAwaiter().GetResult();
+
+        return base.SavingChanges(eventData, result);
+
+    }
+
+    public override async ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        await DispatchDomainEvents(eventData.Context);
+
+        return await base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public async Task DispatchDomainEvents(DbContext? context)
+    {
+        if (context == null) return;
+
+        var entities = context.ChangeTracker
+            .Entries<BaseEntity<object>>()
+            .Where(e => e.Entity.DomainEvents.Count != 0)
+            .Select(e => e.Entity);
+
+        IEnumerable<BaseEntity<object>> baseEntities = entities as BaseEntity<object>[] ?? entities.ToArray();
+        var domainEvents = baseEntities
+            .SelectMany(e => e.DomainEvents)
+            .ToList();
+
+        baseEntities.ToList().ForEach(e => e.ClearDomainEvents());
+
+        foreach (var domainEvent in domainEvents)
+            await _mediator.Publish(domainEvent);
+    }
+}
+
+>>>>>>>

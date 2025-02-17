57700b7f330a0a739486a5a7ef518cf6e53e271e
Ninism <nin.alamo@outlook.com>
2025-02-17 17:47:10 +0800

    Added events

---
+++ espasyo.Domain/Events/IncidentCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using espasyo.Domain.Common;
+
+namespace espasyo.Domain.Events;
+
+public class IncidentCreatedEvent(string caseId, string address) : BaseEvent
+{
+    public string CaseId { get;  } = caseId;
+    public string Address { get; } = address;
+}

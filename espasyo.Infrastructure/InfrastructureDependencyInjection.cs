0a99807dbd7e34c7e7deafeb6353708dd34662b4
Ninism <nin.alamo@outlook.com>
2025-02-17 17:28:13 +0800

    Added interceptor

--- espasyo.Infrastructure/InfrastructureDependencyInjection.cs
+++ espasyo.Infrastructure/InfrastructureDependencyInjection.cs
@@ -1,7 +1,9 @@
 ﻿using espasyo.Application.Common.Interfaces;
 using espasyo.Infrastructure.Data;
+using espasyo.Infrastructure.Data.Interceptors;
 using espasyo.Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace espasyo.Infrastructure;
 
@@ -9,8 +11,16 @@ public static class InfrastructureDependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new Exception("No connection string");
+
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        
+        services.AddDbContext<ApplicationDbContext>((serviceProvider, options) =>
+        {
+            options.AddInterceptors(serviceProvider.GetService<ISaveChangesInterceptor>());
+            options.UseSqlServer(connectionString);
+        });
+
 
         services.AddScoped<IProductRepository, ProductRepository>();
         services.AddScoped<IIncidentRepository, IncidentRepository>();

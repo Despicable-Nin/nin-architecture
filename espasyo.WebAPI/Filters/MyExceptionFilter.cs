1f1d05001d21676ac8faf9957d15d9ea96f98e8b
Ninism <nin.alamo@outlook.com>
2025-02-20 16:14:17 +0800

    Addresss Generator

---
+++ espasyo.WebAPI/Filters/MyExceptionFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace espasyo.WebAPI.Filters;
+
+public class MyExceptionFilter(ILogger<MyExceptionFilter> logger) : IExceptionFilter
+{
+    private readonly ILogger<MyExceptionFilter> _logger = logger;
+
+    public void OnException(ExceptionContext context)
+    {
+
+        var exception = context.Exception;
+        var response = context.HttpContext.Response;
+        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        response.ContentType = "application/json";
+        
+        var errorResponse = new
+        {
+            Message = "An unexpected error occurred. Please try again later.",
+            Detailed = "See logs for more details."
+        };
+
+        if (exception is InvalidOperationException && exception.Source == "Microsoft.ML.KMeansClustering")
+        {
+            //override the status code
+            response.StatusCode = (int)HttpStatusCode.BadRequest;
+            errorResponse = errorResponse with { Message = "Range of date insufficient for clustering. Please extend date range."};
+        }
+      
+        context.Result = new JsonResult(errorResponse);
+
+    }
+}

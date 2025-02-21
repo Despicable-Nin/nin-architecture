b533e952cfa37fee3b90aa16b2f96283e65de896
Ninism <nin.alamo@outlook.com>
2025-02-21 21:05:32 +0800

    Fixed ML

---
+++ espasyo_console/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace espasyo_console;
+
+using System;
+
+public static class EnumHelper
+{
+    private static readonly Random Random = new();
+
+    public static T? GetRandomEnumValue<T>() where T : Enum
+    {
+        var values = Enum.GetValues(typeof(T));
+        return (T)values.GetValue(Random.Next(values.Length))!;
+    }
+}
+
+

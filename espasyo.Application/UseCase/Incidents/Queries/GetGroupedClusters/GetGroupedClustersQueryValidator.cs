f9560042bc358aba48471b4d73432337479079b2
ninalamo <nin.alamo@outlook.com>
2026-06-27 20:50:51 +0800

    updated clustered result validation

--- espasyo.Application/UseCase/Incidents/Queries/GetGroupedClusters/GetGroupedClustersQueryValidator.cs
+++ espasyo.Application/UseCase/Incidents/Queries/GetGroupedClusters/GetGroupedClustersQueryValidator.cs
@@ -20,8 +20,20 @@ public class GetGroupedClustersQueryValidator : AbstractValidator<GetGroupedClus
             .Must(x => !x.DateFrom.HasValue || !x.DateTo.HasValue || x.DateFrom <= x.DateTo)
             .WithMessage("DateFrom must be less than or equal to DateTo.");
 
+        RuleFor(x => x.Features)
+            .NotEmpty()
+            .WithMessage("Features must contain at least Latitude and Longitude.")
+            .Must(x => x.Contains("Latitude") && x.Contains("Longitude"))
+            .WithMessage("Features must contain both 'Latitude' and 'Longitude'.");
 
+        RuleFor(x => x.Filters.CrimeTypes)
+            .Empty()
+            .When(x => x.Features is null || !x.Features.Contains("CrimeType"))
+            .WithMessage("CrimeTypes filter must be empty when CrimeType is not in Features.");
+
+        RuleFor(x => x.Filters.Severities)
+            .Empty()
+            .When(x => x.Features is null || !x.Features.Contains("Severity"))
+            .WithMessage("Severities filter must be empty when Severity is not in Features.");
     }
-
-   
 }
